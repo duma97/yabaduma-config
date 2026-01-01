a1b366265275a064c9f66e8a65b80fffeac3c2ee
duma799 <gptduma@gmail.com>
2026-01-01 13:50:05 +0400

    Update sketchybar color scheme with pywal integration

--- sketchybar/colors.sh
+++ sketchybar/colors.sh
@@ -5,16 +5,28 @@ hex_to_argb() {
     echo "0xff${hex}"
 }
 
+lighten_hex() {
+    local hex="${1#\#}"
+    local amount="${2:-20}"
+    local r=$((16#${hex:0:2}))
+    local g=$((16#${hex:2:2}))
+    local b=$((16#${hex:4:2}))
+    r=$(( r + amount > 255 ? 255 : r + amount ))
+    g=$(( g + amount > 255 ? 255 : g + amount ))
+    b=$(( b + amount > 255 ? 255 : b + amount ))
+    printf "0xff%02x%02x%02x" $r $g $b
+}
+
 if [ -f ~/.cache/wal/colors.sh ]; then
     source ~/.cache/wal/colors.sh
 
-    export BAR_COLOR=$(hex_to_argb "$background")
-    export ITEM_BG_COLOR=$(hex_to_argb "$color0")
+    export BAR_COLOR=$(lighten_hex "$background" 25)
+    export ITEM_BG_COLOR=$(lighten_hex "$background" 25)
     export ACCENT_COLOR=$(hex_to_argb "$color1")
-    export ICON_COLOR=$(hex_to_argb "$foreground")
-    export LABEL_COLOR=$(hex_to_argb "$foreground")
-    export POPUP_BACKGROUND_COLOR=$(hex_to_argb "$color0")
-    export POPUP_BORDER_COLOR=$(hex_to_argb "$color8")
+    export ICON_COLOR=$(hex_to_argb "$color4")
+    export LABEL_COLOR=$(hex_to_argb "$color6")
+    export POPUP_BACKGROUND_COLOR=$(lighten_hex "$background" 20)
+    export POPUP_BORDER_COLOR=$(hex_to_argb "$color1")
     export SHADOW_COLOR="0x80000000"
 else
     export BAR_COLOR="0x00000000"
